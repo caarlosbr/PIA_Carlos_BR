d3588635a2c08e768fbef249596b987e52b38d4f
carlos <carlosborregueror@gmail.com>
2025-10-16 16:25:48 +0200

    comentarios añadidos

--- UD1/Estructuras secuenciales/05.py
+++ UD1/Estructuras secuenciales/05.py
@@ -1,18 +1,29 @@
 ### 5. Un ciclista parte de una ciudad A a las HH horas, MM minutos y SS segundos. El tiempo de viaje hasta llegar a otra ciudad B es de T segundos. Escribir un programa que determine la hora de llegada a la ciudad B.
+
+# Guardamos las horas, minutos, y segundos desde que salimos de la ciudad A
 horasA = int(input("Introduce a que hora sales de CiudadA:\n"))
 minutosA = int(input("Introduce a que minuto sales de CiudadA:\n"))
 segundosA = int(input("Introduce a que segundos sales de CiudadA:\n"))
 
+# Mostramos a la hora,minutos y segundos que salimos 
 print("Sales a las:", horasA, "h", minutosA, "mins", segundosA, "sgs")
+
+# guardamos en segundos la hora de ese momento 
 segundosTotalesA = (horasA * 60 * 60) + (minutosA * 60) + segundosA
 
+# le preguntamos al usuario cuantos segundos se tardan en llegar a la ciudad B
 segundosB = int(input("CUantos segundos se tardan en llegar a ciudad B: \n"))
 
+# guardamos la hora de llegada en segundos
 segundosTotalesSumar = segundosTotalesA + segundosB
 print(segundosTotalesSumar)
 
+# guardamos la hora de llegada en entero 
 horaLlegada = segundosTotalesSumar // 3600
+# guardamos los minutos de llegada 
 minutosLlegada = int((segundosTotalesSumar % 3600) / 60)
+# guardamos los segundos de llegada 
 segundosLlegada = segundosTotalesSumar % 60
 
+# Mostramos a que hora llegaria
 print("Llegas a las:", horaLlegada, "h", minutosLlegada, "mins", segundosLlegada, "sgs")
--- UD1/Estructuras secuenciales/06.py
+++ UD1/Estructuras secuenciales/06.py
@@ -5,16 +5,36 @@
 ## Cada respuesta en blanco suma 0 puntos. 
 ## Imprime la puntuación total obtenida por el estudiante y su nota normalizada entre 0 y 10.
 
-preguntasCorrectas = int(input("Cuantas preguntas correctas tuviste:\n"))
-preguntasSin = int(input("Cuantas preguntas sin responder tuviste:\n"))
-preguntasIncorrectas = int(input("Cuantas preguntas incorrcetas tuviste:\n"))
+# Creamos un while para que en caso de que el usuario introduzca un total de preguntas diferente a 10
+# de error y tenga que meter obligatoriamente 10 preguntas, ya que mi examen tiene 10 preguntas
+while True:
 
-total = 0
+    # Le preguntarmos al usuarios cuantas preguntas bien, sin responder, e incorrectas
+    preguntasCorrectas = int(input("Cuantas preguntas correctas tuviste:\n"))
+    preguntasSin = int(input("Cuantas preguntas sin responder tuviste:\n"))
+    preguntasIncorrectas = int(input("Cuantas preguntas incorrcetas tuviste:\n"))
 
-puntosCorrectas = preguntasCorrectas * 5
-puntosIncorrectas = preguntasIncorrectas * (-2)
-# print(puntosCorrectas, puntosIncorrectas)
+    # guardamos el numeroPreguntas el numero de las preguntas, para comprobar si es 10
+    numeroPreguntas = preguntasCorrectas + preguntasIncorrectas + preguntasSin
 
-total = puntosCorrectas + puntosIncorrectas
+    # Si es 10 sigue el ciclo del while
+    if numeroPreguntas == 10:
 
-print("Tu nota final es:", total / 5)
+        # Multiplicamos por 5 que es lo que vale cada pregunta bien
+        puntosCorrectas = preguntasCorrectas * 5
+
+        # Multiplicamos por -2 por cada pregunta mal
+        puntosIncorrectas = preguntasIncorrectas * (-2)
+        # print(puntosCorrectas, puntosIncorrectas)
+
+        # Guardamos en total todos los puntos totales
+        total = puntosCorrectas + puntosIncorrectas
+
+        # Imprimos la nota final a un valor de 0 a 10
+        print("Tu nota final es:", total / 5)
+        break
+    # Si el valor de numero preguntas es diferente a 10 repetimos el bucle y mostramos el mensaje
+    else: 
+        print("Debes de introducir 10 preguntas en total, ya que el examen tiene 10 preguntas")
+
+
